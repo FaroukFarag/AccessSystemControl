8cd975717ec696bcfe7fe64b2cc26b8ea98948cc
Shaimaa Gamal <shaimaagamal131@gmail.com>
2025-04-19 16:36:25 +0200

    latest version

--- AccessControlSystem.Domain/obj/Debug/net8.0/AccessControlSystem.Domain.AssemblyInfo.cs
+++ AccessControlSystem.Domain/obj/Debug/net8.0/AccessControlSystem.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessControlSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d710fbcfbd20d8c9dc536c1250139b6d86796c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+019438a879e7a0487d933ad343f9094cd428211e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessControlSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessControlSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
