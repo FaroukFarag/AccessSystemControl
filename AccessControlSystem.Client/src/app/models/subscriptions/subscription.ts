19b0d0c3b2942da31cc93b5926fc56c0225e12cf
Farouk Farag <farouk.farag98@gmail.com>
2025-04-28 18:13:22 +0200

    Add Admin and Card Number in Subscription.

--- AccessControlSystem.Client/src/app/models/subscriptions/subscription.ts
+++ AccessControlSystem.Client/src/app/models/subscriptions/subscription.ts
@@ -3,7 +3,12 @@ import { BaseModel } from "../shared/base-model.model";
 export class Subscription extends BaseModel<number> {
   customerName!: string;
   subscriptionType!: number;
+  adminNumber!: number;
   deviceNumber!: number;
+  cardNumber!: number;
+  usedAdmins!: number;
+  usedDevices!: number;
+  usedCards!: number;
   paymentPerMonth!: number;
   startDate!: Date;
   endDate!: Date;
