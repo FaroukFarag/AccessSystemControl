3fb4376c52d7b598deb36d681cff59f200d77956
Farouk Farag <farouk.farag98@gmail.com>
2025-05-09 12:54:39 +0200

    Include Devices in AccessGroup Get.

--- AccessControlSystem.Application/Dtos/AccessGroups/AccessGroupDto.cs
+++ AccessControlSystem.Application/Dtos/AccessGroups/AccessGroupDto.cs
@@ -1,8 +1,11 @@
 ﻿using AccessControlSystem.Application.Dtos.Abstraction;
+using AccessControlSystem.Application.Dtos.Devices;
 
 namespace AccessControlSystem.Application.Dtos.AccessGroups;
 
 public class AccessGroupDto : BaseModelDto<int>
 {
     public string Name { get; set; } = default!;
+
+    public IEnumerable<DeviceDto>? Devices { get; set; }
 }
--- AccessControlSystem.Application/Services/AccessGroups/AccessGroupService.cs
+++ AccessControlSystem.Application/Services/AccessGroups/AccessGroupService.cs
@@ -3,7 +3,9 @@ using AccessControlSystem.Application.Interfaces.AccessGroups;
 using AccessControlSystem.Application.Services.Abstraction;
 using AccessControlSystem.Domain.Interfaces.Repositories.AccessGroups;
 using AccessControlSystem.Domain.Interfaces.UnitOfWork;
+using AccessControlSystem.Domain.Models.AccessGroupDevices;
 using AccessControlSystem.Domain.Models.AccessGroups;
+using AccessControlSystem.Domain.Specifications.Absraction;
 using AutoMapper;
 
 namespace AccessControlSystem.Application.Services.AccessGroups;
@@ -15,4 +17,20 @@ public class AccessGroupService(
     BaseService<AccessGroup, AccessGroupDto, int>(repository, unitOfWork, mapper),
     IAccessGroupService
 {
+    private readonly IAccessGroupRepository _repository = repository;
+    private readonly IMapper _mapper = mapper;
+
+    public async override Task<AccessGroupDto> GetAsync(int id)
+    {
+        var accessGroup = await _repository.GetAsync(id, new BaseSpecification<AccessGroup>
+        {
+            Includes = [ag => ag.AccessGroupDevices],
+            IncludesThen = [
+                (ag => ag.AccessGroupDevices, sd => (sd as AccessGroupDevice)!.Device)
+            ]
+        });
+        var subscriptionDto = _mapper.Map<AccessGroupDto>(accessGroup);
+
+        return subscriptionDto;
+    }
 }
