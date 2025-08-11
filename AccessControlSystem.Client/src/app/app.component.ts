597284dba186ad3b9ee8d3ccd238f1f67d375c81
Shaimaa Gamal <shaimaagamal131@gmail.com>
2025-08-12 02:21:40 +0300

    Localization Implementation

--- AccessControlSystem.Client/src/app/app.component.ts
+++ AccessControlSystem.Client/src/app/app.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    SidebarComponent
   ],
-  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WS';
+export class AppComponent implements OnInit {
+  showLayout = true;
+
+  constructor(private router: Router) {
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Check auth state after navigation
+      this.checkAuthState();
+    });
+  }
+
+  ngOnInit() {
+    this.checkAuthState();
+  }
+
+  checkAuthState() {
+    const token = localStorage.getItem('authToken');
+    this.showLayout = !!token && !this.router.url.includes('/login');
+  }
+
+  onLogout() {
+    this.showLayout = false;
+  }
 }
