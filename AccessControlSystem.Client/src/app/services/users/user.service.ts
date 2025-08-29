224e8e635b46d044f014838f25bae572593b593b
Shaimaa Gamal <shaimaagamal131@gmail.com>
2025-08-29 17:54:16 +0300

    services  and localizations

--- AccessControlSystem.Client/src/app/services/users/user.service.ts
+++ AccessControlSystem.Client/src/app/services/users/user.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from '../shared/base-service.service';
 import { User } from '../../models/users/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService  extends BaseService<User>{
+export class UserService extends BaseService<User>{
   constructor() { 
     super();
+  }
+
+  upgradeSubscription(endpoint: string, data: any): Observable<any> {
+    return this.http.patch(`${this.baseUrl}/${endpoint}`, data);
+  }
+
+  // Generic POST for actions that are not full User payloads (e.g., cancel/delete by id)
+  postAction(endpoint: string, data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/${endpoint}`, data);
   }
 }
